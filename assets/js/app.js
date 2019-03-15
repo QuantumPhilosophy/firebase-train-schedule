66bbdf0e0f3aede478ef1132012df617849d01e1
QuantumPhilosopher <16587451+QuantumPhilosophy@users.noreply.github.com>
2019-03-14 22:27:03 -0700

    adding Firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 'use strict'
+
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyAZbXq8IYq1HcTReBlOrwjsQk6tHUvB_M8',
+  authDomain: 'uw-bootcamp-f90ad.firebaseapp.com',
+  databaseURL: 'https://uw-bootcamp-f90ad.firebaseio.com',
+  projectId: 'uw-bootcamp-f90ad',
+  storageBucket: 'uw-bootcamp-f90ad.appspot.com',
+  messagingSenderId: '322632099797'
+}
+firebase.initializeApp(config)
